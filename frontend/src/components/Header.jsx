2ebcc99278add18c0c23925313d38ebc4374979e
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-12-05 18:18:14 +0300

    Chore: update component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,18 +2,17 @@ import { Navbar, Button, Container } from 'react-bootstrap';
 
 import { useAuth } from '../hooks';
 
-const ChatNavbar = () => {
+const Header = () => {
   const auth = useAuth();
 
   return (
     <Navbar className="shadow-sm" variant="light" bg="white" expand="lg">
       <Container>
         <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         {auth.loggedIn && <Button onClick={auth.logOut} variant="primary">Выйти</Button>}
       </Container>
     </Navbar>
   );
 };
 
-export default ChatNavbar;
+export default Header;
