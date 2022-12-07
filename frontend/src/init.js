7edefcdd82d7b75e5a3b8626e826bd44a78d5cca
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-12-07 16:36:57 +0300

    Chore: add rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -6,6 +6,8 @@ import {
 import { Provider } from 'react-redux';
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import Rollbar from 'rollbar';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,6 +18,14 @@ import ChatProvider from './providers/ChatProvider.jsx';
 import store from './slices/index.js';
 import AppRoutes from './AppRoutes.jsx';
 import ru from './locales/ru.js';
+
+const rollbarConfig = {
+  accessToken: 'b0e70c8e768e4312945bbdec3b535648',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+const rollbar = new Rollbar(rollbarConfig);
 
 const init = async () => {
   const defaultLanguage = 'ru';
@@ -30,28 +40,32 @@ const init = async () => {
 
   const rootNode = ReactDOM.createRoot(document.getElementById('root'));
   rootNode.render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <AuthProvider>
-          <ChatProvider>
-            <AppRoutes />
-            <ToastContainer
-              position="top-right"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-            <ModalWindow />
-          </ChatProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </Provider>,
+    <RollbarProvider instance={rollbar}>
+      <ErrorBoundary errorMessage="Rollbar error in my app">
+        <Provider store={store}>
+          <BrowserRouter>
+            <AuthProvider>
+              <ChatProvider>
+                <AppRoutes />
+                <ToastContainer
+                  position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+                <ModalWindow />
+              </ChatProvider>
+            </AuthProvider>
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>,
   );
 };
 
