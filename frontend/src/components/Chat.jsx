4a81ea115649dd78bdfa2935a70f6e7e32dafc77
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-12-08 14:23:18 +0300

    Feat: add # to channel name

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -118,7 +118,7 @@ const Chat = () => {
             <div className="col p-0 h-100">
               <div className="d-flex flex-column h-100">
                 <div className="bg-light mb-4 p-3 shadow-sm small">
-                  <p className="m-0"><b>{currentChannel && currentChannel.name}</b></p>
+                  <p className="m-0"><b>{`# ${currentChannel.name}`}</b></p>
                   <span className="text-muted">
                     {t('components.chat.messages.message', { count: currentChannelMessages.length })}
                   </span>
